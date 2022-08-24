15c9aa30627197351e4e42ca463e41d9121e5f7f
efimero <martinperez9713@gmail.com>
2022-08-23 21:04:46 -0300

    update NavBarPc

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
 import { NavBarResponsive } from "./components/NavBarResponsive/NavBarResponsive";
-import { Home } from "./pages/Home/Home";
 import { SobreMi } from "./pages/Sobre mi/SobreMi";
 import { Encontrar } from "./pages/Encontrar/Encontrar";
 import { Libros } from "./pages/Libros/Libros";
 import { BoxMultisensorial } from "./pages/BoxMultisensorial/BoxMultisensorial";
 import { Contactame } from "./pages/Contactame/Contactame";
 import { Footer } from "./components/Footer/Footer";
+import { ThemeProvider } from "./context/themes";
 
 function App() {
   return (
     <>
       <BrowserRouter>
-        <NavBarResponsive />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/sobre-mi" element={<SobreMi />} />
-          <Route path="/que-vas-a-encontrar" element={<Encontrar />} />
-          <Route path="/libros" element={<Libros />} />
-          <Route path="/box-multisensorial" element={<BoxMultisensorial />} />
-          <Route path="/contactame" element={<Contactame />} />
-        </Routes>
-        <Footer />
+        <ThemeProvider>
+          <NavBarResponsive />
+          <Routes>
+            <Route path="/" element={<SobreMi />} />
+            <Route path="/que-vas-a-encontrar" element={<Encontrar />} />
+            <Route path="/libros" element={<Libros />} />
+            <Route path="/box-multisensorial" element={<BoxMultisensorial />} />
+            <Route path="/contactame" element={<Contactame />} />
+            <Route path="*" element={<Navigate to="/" replace />} />
+          </Routes>
+          <Footer />
+        </ThemeProvider>
       </BrowserRouter>
     </>
   );
